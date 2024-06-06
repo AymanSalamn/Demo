fd5e282892c41b4f3174db68e30f5d1e3238520f
AymanSalamn <91143188+AymanSalamn@users.noreply.github.com>
2024-06-06 13:08:40 +0300

    Update jenkin_script.sh

--- jenkin_script.sh
+++ jenkin_script.sh
@@ -2,27 +2,6 @@
 
 # Process and Upload Report
 
-# Clone repository
-git clone https://github.com/${{ github.repository }}.git
-cd $(basename "${{ github.repository }}")
-
-# Set up Python
-sudo apt update
-sudo apt install python3.9 python3-pip -y
-
-# Install Java
-sudo apt update
-sudo apt install default-jdk -y
-
-# Download and Install Allure
-wget https://github.com/allure-framework/allure2/releases/download/2.27.0/allure-2.27.0.tgz
-sudo tar -zxvf allure-2.27.0.tgz -C /opt/
-sudo ln -s /opt/allure-2.27.0/bin/allure /usr/bin/allure
-
-# Install dependencies
-python3 -m pip install --upgrade pip
-pip install -r requirements.txt
-
 # Generate report
 pytest --alluredir=ReportResultsFilesTest
 
