9d87d6d0d0f9f506d08b09a8b25baa3cdfc0b2bd
AymanSalamn <91143188+AymanSalamn@users.noreply.github.com>
2024-06-06 17:02:45 +0300

    Update jenkins_job.sh

--- jenkins_job.sh
+++ jenkins_job.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-# Run Tests
-pytest --alluredir=report_results_json
 
-# Generate Allure Report 
-allure generate --clean --single-file report_results_json -o report_results_html
+# install java
+sudo apt update
+sudo apt install default-jdk -y
 
-# Compressed Report Results
-zip -r report_results.zip report_results_html
+# install allure command
+wget https://github.com/allure-framework/allure2/releases/download/2.27.0/allure-2.27.0.tgz
+sudo tar -zxvf allure-2.27.0.tgz -C /opt/
+sudo ln -s /opt/allure-2.27.0/bin/allure /usr/bin/allure 
+allure --version
 
 
-# Set Directory name
-DIR_NAME=$(date +%Y-%m-%d)
-mkdir -p "$DIR_NAME"
-cd "$DIR_NAME"
-SUB_DIR=$(date +%H-%M-%S)
-mkdir -p "$SUB_DIR"
-cd ..
+# # Run Tests
+# pytest --alluredir=report_results_json
 
-# Move Directory
-mv report_results.zip "$DIR_NAME/$SUB_DIR"
+# # Generate Allure Report 
+# allure generate --clean --single-file report_results_json -o report_results_html
+
+# # Compressed Report Results
+# zip -r report_results.zip report_results_html
+
+
+# # Set Directory name
+# DIR_NAME=$(date +%Y-%m-%d)
+# mkdir -p "$DIR_NAME"
+# cd "$DIR_NAME"
+# SUB_DIR=$(date +%H-%M-%S)
+# mkdir -p "$SUB_DIR"
+# cd ..
+
+# # Move Directory
+# mv report_results.zip "$DIR_NAME/$SUB_DIR"
