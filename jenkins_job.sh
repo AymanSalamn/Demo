70814d86bc36d791336208b5834211fb10dc6a6b
AymanSalamn <91143188+AymanSalamn@users.noreply.github.com>
2024-06-08 16:45:38 +0300

    Update jenkins_job.sh

--- jenkins_job.sh
+++ jenkins_job.sh
@@ -6,8 +6,8 @@ apt install default-jdk -y
 
 # install allure command
 wget https://github.com/allure-framework/allure2/releases/download/2.27.0/allure-2.27.0.tgz
-tar -zxvf allure-2.27.0.tgz -C /opt/
-ln -s /opt/allure-2.27.0/bin/allure /usr/bin/allure 
+sudo tar -zxvf allure-2.27.0.tgz -C /opt/
+ln -sf  /opt/allure-2.27.0/bin/allure /usr/bin/allure 
 allure --version
 
 
