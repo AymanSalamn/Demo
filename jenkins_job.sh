bb76bf4e8868b51bddd33e72432f792f294f3be2
AymanSalamn <91143188+AymanSalamn@users.noreply.github.com>
2024-06-09 11:55:57 +0300

    Update jenkins_job.sh

--- jenkins_job.sh
+++ jenkins_job.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-
-# install java
-apt install default-jdk -y
-
-
+# Install Allure 
 current_dir=$(pwd)
 wget https://github.com/allure-framework/allure2/releases/download/2.27.0/allure-2.27.0.tgz
 tar -zxvf allure-2.27.0.tgz -C "$current_dir"
 allure_dir="$current_dir/allure-2.27.0"
 #ln -sf "$allure_dir"/bin/allure /usr/bin/allure
-
-# Check Allure version
-"$allure_dir"/bin/allure --version
-
 
 # Run Tests
 #pytest --alluredir=report_results_json
@@ -29,7 +21,6 @@ report_results_dir_html="${results_dir_name}_html"
 # Compressed Report Results
 zip -r "${report_results_dir_html}.zip" $report_results_dir_html
 
-
 # Set Directory name
 DIR_NAME=$(date +%Y-%m-%d)
 mkdir -p "$DIR_NAME"
@@ -41,18 +32,14 @@ cd ..
 # Move Directory
 mv "${report_results_dir_html}.zip" "$DIR_NAME/$SUB_DIR"
 
+# printenv
 
-printenv
 # Upload to S3
 AWS_S3_BUCKET="pytest-allure-report-results"
 AWS_REGION="us-east"
 
-echo "$AWS_ACCESS_KEY"
-echo "$AWS_SECRET_KEY"
-
 export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY"
 export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_KEY"
-
 
 aws s3 cp "$DIR_NAME/$SUB_DIR/${report_results_dir_html}.zip" s3://"$AWS_S3_BUCKET"/"$DIR_NAME"/"$SUB_DIR/"
 
