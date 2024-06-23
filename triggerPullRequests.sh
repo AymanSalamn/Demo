872da099aafb901072137f689c963a90e8929a23
AymanSalamn <91143188+AymanSalamn@users.noreply.github.com>
2024-06-23 16:53:03 +0300

    Update triggerPullRequests.sh

--- triggerPullRequests.sh
+++ triggerPullRequests.sh
@@ -1,1 +1,21 @@
-pytest
+pipeline {
+    agent any
+    stages {
+        stage('Execute Commands') {
+            steps {
+                // Replace with your actual commands to be executed
+                sh "mkdir testing"
+            }
+        }
+
+        // Add more stages as needed
+    }
+
+    post {
+        always {
+            // Clean up or post-processing steps can go here
+            deleteDir()
+        }
+    }
+
+}
